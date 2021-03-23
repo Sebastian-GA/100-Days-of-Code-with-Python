651e482888fd192b3df631e1bfc13b11be1b514f
Sebastián García <isebastian.ga@gmail.com>
2021-03-22 20:13:50 -0500

    correction of tic-tac-toe program
    There were a problem when the program checked for horizontal lines.
    Now the program can check vertical lines and it's finished.

--- Extras/Tic-Tac-Toe/main.py
+++ Extras/Tic-Tac-Toe/main.py
@@ -3,8 +3,9 @@
 # There are two players 'X' and 'O' and the first one who
 # make a line of 3 wins.
 
-table = list("_________")
+table = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
 player_ = True  # If it's True is the turn of X player
+
 
 def player():
     if player_:
@@ -12,58 +13,66 @@ def player():
     else:
         return "O"
 
+
 def print_table():
     print("---------")
-    print(f"| {table[0]} {table[1]} {table[2]} |")
-    print(f"| {table[3]} {table[4]} {table[5]} |")
-    print(f"| {table[6]} {table[7]} {table[8]} |")
+    for row in table:
+        print(f"| {row[0]} {row[1]} {row[2]} |")
     print("---------")
+
 
 def insert_player():
     while True:
-        position = list(input(f"What position will player {player()} play (x y): "))
-        if (len(position) != 3) or position[1] != " ":
+        position = input(f"What position will player {player()} play (x y): Numbers must be 1, 2 or 3 ").split()
+        if len(position) != 2:
             print("error: You should insert two numbers separated by an space")
         else:
-            x = int(position[0])
-            y = int(position[2])
-            if not(0 <= x < 3) or not(0 <= y < 3):
-                print("error: Invalid position. Numbers must be 0, 1 or 2")
-            elif table[x + y*3] != "_":  # That space is occuped
-                print("error: Invalid position. That position is alredy occuped")
+            x = int(position[0]) - 1
+            y = int(position[1]) - 1
+            if not (0 <= x < 3) or not (0 <= y < 3):
+                print("error: Invalid position. Numbers must be 1, 2 or 3")
+            elif table[y][x] != "_":
+                print("error: Invalid position. That position is already occupied")
             else:
-                table[x + y*3] = player()
+                table[y][x] = player()
                 global player_
                 player_ = not player_  # It's the turn of the other player
                 break
-    
-    return 0
+
 
 def check_table():
-    if "'X', 'X', 'X'" in str(table):  # Check horizontal lines
+    if ["X", "X", "X"] in table:  # Check horizontal lines
         print("Player X won")
         return 1
-    elif "'O', 'O', 'O'" in str(table):
+    elif ["O", "O", "O"] in table:
         print("Player O won")
         return 1
-    elif table[0] == table [4] == table[8] == "X":  # Check principal diagonal
+    elif table[0][0] == table[1][1] == table[2][2] == "X":  # Check principal diagonal
         print("Player X won")
         return 1
-    elif table[0] == table [4] == table[8] == "O":
+    elif table[0][0] == table[1][1] == table[2][2] == "O":
         print("Player O won")
         return 1
-    elif table[2] == table [4] == table[6] == "X":  # Check the other diagonal
+    elif table[0][2] == table[1][1] == table[2][0] == "X":  # Check the other diagonal
         print("Player X won")
         return 1
-    elif table[2] == table [4] == table[6] == "O":
+    elif table[0][2] == table[1][1] == table[2][0] == "O":
         print("Player O won")
         return 1
-    else:
-        
-        # Check vertical lines
+    else:  # Check vertical lines
+        for col in range(3):
+            if table[0][col] == table[1][col] == table[2][col] == "X":
+                print("Player X won")
+                return 1
+            elif table[0][col] == table[1][col] == table[2][col] == "O":
+                print("Player O won")
+                return 1
+            else:
+                print("The match continues")
+                return 0
 
-        print("The match continues")
-        return 0
+
+print("Welcome to Tic-Tac-Toe")
 
 while True:
     insert_player()
