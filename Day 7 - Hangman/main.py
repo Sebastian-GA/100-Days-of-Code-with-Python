0f2ae2242353e8e4581972e21ce2b9accb9ac534
Sebastián García <isebastian.ga@gmail.com>
2021-03-25 18:53:29 -0500

    day 7 proyect finished
    Clear terminal, print ascii art and minecraft thematic

--- Day 7 - Hangman/main.py
+++ Day 7 - Hangman/main.py
@@ -1,6 +1,10 @@
 # Day 7 - Hangman
 
 import random
+from ascii_art import ascii_hangman, hangman_title
+from words import words_list
+import os
+def clear(): os.system('cls')  # Clear terminal on windows
 
 def finished():
     if "".join(user_word) == word:
@@ -8,33 +12,40 @@ def finished():
         return True
     elif lives == 0:
         print("You lose")
+        print(f"The word was {word}")
         return True
     else:
         return False
 
 def print_hangman():
-    print(f"Your score {lives}")
+    # print(f"Your score {lives}")
+    print(ascii_hangman[lives])
+
 
 # Configuration
-words_list = ["horse", "house", "university", "python", "apple", "sausage"]
-word = random.choice(words_list)
+word = random.choice(words_list).lower()
 
 user_word = []
 for n in range(len(word)):
     user_word.append("_")
 
-lives = 10
+lives = 6
 letter = ""
-used_letters = []
+used_letters = []  # Letters that have been entered
 
 # Game
+clear()
+print("Welcome to")
+print(hangman_title)
+
 while not finished():
-    letter = input("Guess a letter: ")
+    letter = input("Guess a letter: ").lower()
     # Check input
     while len(letter) != 1 or letter in "1234567890!\"#$%&/()=?¿'*+-_]}[{^;,:.":
         print("error: Incorrect input. The input must be a letter.")
-        letter = input("Try again. Guess a letter: ")
-    
+        letter = input("Try again. Guess a letter: ").lower()
+    clear()
+
     # Check letter
     if letter in word and letter not in user_word:
         used_letters.append(letter)
